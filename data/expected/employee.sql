83f13a794a46295fb4de70daac61e9a832f80603
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-13 07:52:00 +0000

    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@45 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- data/expected/employee.sql
+++ data/expected/employee.sql
@@ -1,4 +1,4 @@
-insert into EMPLOYEE (ID, NAME) values (1, Fred);
-insert into EMPLOYEE (ID, NAME) values (2, Wilma);
-insert into EMPLOYEE (ID, NAME) values (3, Barney);
-insert into EMPLOYEE (ID, NAME) values (4, Betty);
+insert into EMPLOYEE (ID, NAME) values (1, 'Fred');
+insert into EMPLOYEE (ID, NAME) values (2, 'Wilma');
+insert into EMPLOYEE (ID, NAME) values (3, 'Barney');
+insert into EMPLOYEE (ID, NAME) values (4, 'Betty');
