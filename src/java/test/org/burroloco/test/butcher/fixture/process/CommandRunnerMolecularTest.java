fffd41d9c0145e861f467da1934276422b49e569
paulk <paulk@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-15 05:51:53 +0000

    Windoweasel test
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@64 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/test/org/burroloco/test/butcher/fixture/process/CommandRunnerMolecularTest.java
+++ src/java/test/org/burroloco/test/butcher/fixture/process/CommandRunnerMolecularTest.java
@@ -3,19 +3,37 @@ package org.burroloco.test.butcher.fixture.process;
 import org.burroloco.test.glue.testcase.DonkeyTestCase;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import au.net.netstorm.boost.spider.api.lifecycle.Constructable;
 
-public class CommandRunnerMolecularTest extends DonkeyTestCase {
+public class CommandRunnerMolecularTest extends DonkeyTestCase implements Constructable {
     private static final int EVERYTHING_COOL_MAN = 0;
     private static final int NOT_GOOD = 1;
+    private static final String COMMAND_COOL = "exit 0";
+    private static final String COMMAND_BAD = "exit 1";
+    private static final String WINDOWS_CMD = "cmd.exe,/C,";
+    private static final String UNIX_CMD = "sh,-c,";
+    private String[] commandCool;
+    private String[] commandBad;
     CommandRunner runner;
 
+
     public void testOk() throws Exception {
-        int returnCode = runner.run("sh", "-c", "exit 0");
+        int returnCode = runner.run(commandCool);
         assertEquals(EVERYTHING_COOL_MAN, returnCode);
     }
 
     public void testFail() throws Exception {
-        int returnCode = runner.run("sh", "-c", "exit " + NOT_GOOD);
+        int returnCode = runner.run(commandBad);
         assertThat(returnCode, equalTo(NOT_GOOD));
+    }
+
+    public void constructor() {
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            commandCool = (WINDOWS_CMD + COMMAND_COOL).split(",");
+            commandBad = (WINDOWS_CMD + COMMAND_BAD).split(",");
+        } else {
+            commandCool = (UNIX_CMD + COMMAND_COOL).split(",");
+            commandBad = (UNIX_CMD + COMMAND_BAD).split(",");
+        }
     }
 }
