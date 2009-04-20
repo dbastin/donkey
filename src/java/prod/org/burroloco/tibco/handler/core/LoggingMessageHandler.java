136607551109ec18f5043c9ec48fafc21c25091e
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-20 06:54:25 +0000

    TibcoToFixedWidthDemoTest.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@106 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/tibco/handler/core/LoggingMessageHandler.java
+++ src/java/prod/org/burroloco/tibco/handler/core/LoggingMessageHandler.java
@@ -4,21 +4,12 @@ import au.net.netstorm.boost.bullet.log.Log;
 import edge.com.tibco.tibrv.TibrvListener;
 import edge.com.tibco.tibrv.TibrvMsg;
 
-// FIX TSR-DONKEY Use or Lose
 public class LoggingMessageHandler implements MessageHandler {
     MessageHandler delegate;
     Log log;
 
     public void onMsg(TibrvListener listener, TibrvMsg msg) {
-        log.trace("Received on " + listener.getSubject() + ": " + format(msg));
+        log.trace("Received on " + listener.getSubject() + ": " + msg);
         delegate.onMsg(listener, msg);
     }
-
-    private String format(TibrvMsg msg) {
-        String result = msg.toString();
-        result = result.replaceAll("KPLUSFEED=", "\nKPLUSFEED=");
-        result = result.replaceAll("Table=", "\n\nTable=");
-        return result;
-    }
-
 }
