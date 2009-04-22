064cea5e9d1d6f4a79aa728165af4f17e2c98c00
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-22 03:46:30 +0000

    Renamed MetaDirectoryToFixedWidthDemoTest to FileMetadataToFixedWidthDemoTest.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@119 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/test/org/burroloco/demo/filemeta2fixed/FileMetadataToFixedWidthSpecification.java
+++ src/java/test/org/burroloco/demo/filemeta2fixed/FileMetadataToFixedWidthSpecification.java
@@ -6,10 +6,14 @@ import org.burroloco.donkey.trebuchet.Specification;
 import org.burroloco.donkey.trebuchet.Wirer;
 
 public class FileMetadataToFixedWidthSpecification implements Specification {
+    private static final String[] PROPS = {
+            "config/filemeta2fixed.properties",
+            "config/filemeta2fixed-override.properties"
+    };
     ConfigLoader loader;
 
     public Config config() {
-        return loader.overload("config/filemeta2fixed.properties", "config/filemeta2fixed-override.properties");
+        return loader.overload(PROPS);
     }
 
     public Class<? extends Wirer> wirer() {
