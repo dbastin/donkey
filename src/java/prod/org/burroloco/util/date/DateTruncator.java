8a096dbfedc614ecd195f03f87fe1efdec6ba0ec
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-25 06:13:45 +0000

    Removing unnecessary code.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@135 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/util/date/DateTruncator.java
+++
@@ -1,7 +0,0 @@
-package org.burroloco.util.date;
-
-import org.burroloco.donkey.data.cake.Slice;
-
-public interface DateTruncator {
-    void truncate(Slice noEmpties, Slice dates, String field);
-}
