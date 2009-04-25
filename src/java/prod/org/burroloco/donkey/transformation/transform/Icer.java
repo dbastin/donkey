d3c55ce9bf32e4aec2258ae0de2b704dce9b8b06
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-25 23:54:51 +0000

    Removing unecessary code.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@148 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/donkey/transformation/transform/Icer.java
+++
@@ -1,7 +0,0 @@
-package org.burroloco.donkey.transformation.transform;
-
-import org.burroloco.donkey.data.cake.Cake;
-
-public interface Icer {
-    Cake transform(Cake cake);
-}
--- src/java/prod/org/burroloco/donkey/transformation/transform/XmlSliceCleaner.java
+++
@@ -1,7 +0,0 @@
-package org.burroloco.donkey.transformation.transform;
-
-import org.burroloco.donkey.data.cake.Slice;
-
-public interface XmlSliceCleaner {
-    Slice clean(Slice in);
-}
