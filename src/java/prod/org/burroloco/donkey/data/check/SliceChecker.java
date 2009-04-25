fc7cac6e762ddd6d61234462e841ea82487b2d84
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-25 23:42:48 +0000

    Removing unnecessary code.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@146 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/donkey/data/check/SliceChecker.java
+++
@@ -1,7 +0,0 @@
-package org.burroloco.donkey.data.check;
-
-import org.burroloco.donkey.data.cake.Slice;
-
-public interface SliceChecker {
-    void check(Slice slice, int expected);
-}
--- src/java/prod/org/burroloco/util/currency/CurrencyUtil.java
+++
@@ -1,5 +0,0 @@
-package org.burroloco.util.currency;
-
-public interface CurrencyUtil {
-    String format(String amount, String currencyCode);
-}
