e59a7fad178eca737737b8a54e05af9e53bbddc1
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-26 00:20:16 +0000

    Removing unnecessary code.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@149 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/donkey/input/database/SingleResultQuery.java
+++
@@ -1,9 +0,0 @@
-package org.burroloco.donkey.input.database;
-
-import org.burroloco.config.core.Config;
-import org.burroloco.donkey.data.cake.Slice;
-
-public interface SingleResultQuery {    
-    Object value(Config config, String sqlStatement);
-    Slice slice(Config config, String sqlStatement);
-}
