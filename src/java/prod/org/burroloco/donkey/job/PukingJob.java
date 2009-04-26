2a7079c989835528d422016f0cbf6ba59835d934
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-26 08:06:58 +0000

    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@161 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/donkey/job/PukingJob.java
+++ src/java/prod/org/burroloco/donkey/job/PukingJob.java
@@ -3,8 +3,6 @@ package org.burroloco.donkey.job;
 import org.burroloco.config.core.Config;
 import org.burroloco.donkey.error.listener.core.Puker;
 
-// FIX DONKEY Should this be the default?
-// FIX DONKEY You can control behaviour through Puker wiring. 
 public class PukingJob implements Job {
     Job delegate;
     Puker puker;
