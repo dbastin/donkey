102dc5283c65b39ef52a9e588f588abce5eebbaa
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-27 11:47:14 +0000

    Coverage.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@202 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/donkey/output/csv/DefaultValueProcessor.java
+++ src/java/prod/org/burroloco/donkey/output/csv/DefaultValueProcessor.java
@@ -1,10 +1,7 @@
 package org.burroloco.donkey.output.csv;
 
-import org.burroloco.donkey.input.database.DatabaseNull;
-
 public class DefaultValueProcessor implements ValueProcessor {
     public String string(Object value) {
-        if (value instanceof DatabaseNull) return "";
         return '"' + value.toString() + '"';
     }
 
--- src/java/prod/org/burroloco/donkey/transformation/transform/FieldConverter.java
+++
@@ -1,9 +0,0 @@
-package org.burroloco.donkey.transformation.transform;
-
-import org.burroloco.donkey.data.cake.Slice;
-
-public interface FieldConverter {
-    Slice nullTo(Slice in, Object replacement);
-
-    Slice emptyTo(Slice in, Object replacement);
-}
