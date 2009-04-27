6615ac733c68c7b0f4724c5193eeb33a619896b4
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-27 07:19:01 +0000

    Coverage.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@191 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/test/org/burroloco/donkey/error/DefaultPukerCoverageTest.java
+++ src/java/test/org/burroloco/donkey/error/DefaultPukerCoverageTest.java
@@ -1,29 +1,28 @@
-package org.burroloco.donkey.transformation.error;
+package org.burroloco.donkey.error;
 
 import au.net.netstorm.boost.gunge.exception.ThrowableMaster;
 import au.net.netstorm.boost.sniper.marker.LazyFields;
 import au.net.netstorm.boost.sniper.marker.OverlaysWeb;
 import au.net.netstorm.boost.spider.api.runtime.Nu;
-import org.burroloco.donkey.data.cake.Slice;
+import org.burroloco.config.core.Config;
+import org.burroloco.donkey.error.listener.core.Puker;
 import org.burroloco.test.glue.testcase.DonkeyTestCase;
 
-public class DefaultBurperCoverageTest extends DonkeyTestCase implements LazyFields, OverlaysWeb {
-    //SIMIAN OFF
+public class DefaultPukerCoverageTest extends DonkeyTestCase implements LazyFields, OverlaysWeb {
     ThrowableMaster chuckieMock;
     Throwable causeDummy;
     RuntimeException e;
-    Burper subject;
+    Config configDummy;
+    Puker subject;
     Nu nu;
-    //SIMIAN ON
 
     public void overlay() {
         wire.ref(chuckieMock).to(ThrowableMaster.class);
     }
 
     public void test() {
-        expect.oneCall(chuckieMock, (Object) causeDummy, "realCause", e);
+        expect.oneCall(chuckieMock, (Object) causeDummy, "rootCause", e);
         expect.oneCall(chuckieMock, VOID, "rethrow", causeDummy);
-        Slice slice = nu.nu(Slice.class);
-        subject.error(slice, e);
+        subject.error(configDummy, e);
     }
 }
