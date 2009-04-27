676e2312a46fde29523c2e4518c87162b091c19d
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-27 06:54:51 +0000

    Coverage.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@187 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/donkey/data/cake/DefaultSlicer.java
+++ src/java/prod/org/burroloco/donkey/data/cake/DefaultSlicer.java
@@ -5,15 +5,8 @@ import au.net.netstorm.boost.spider.api.runtime.Nu;
 import java.util.HashMap;
 import java.util.Map;
 
-// FIX TSR-DONKEY Use or Lose
 public class DefaultSlicer implements Slicer {
     Nu nu;
-
-    public Slice copy(Slice slice) {
-        Slice newSlice = nu.nu(Slice.class);
-        newSlice.addAll(slice);
-        return newSlice;
-    }
 
     public Map<String, String> stringMap(Slice slice) {
         Map<String, String> map = new HashMap<String, String>();
--- src/java/prod/org/burroloco/donkey/data/cake/Slicer.java
+++ src/java/prod/org/burroloco/donkey/data/cake/Slicer.java
@@ -3,7 +3,5 @@ package org.burroloco.donkey.data.cake;
 import java.util.Map;
 
 public interface Slicer {
-    Slice copy(Slice slice);
-
     Map<String, String> stringMap(Slice slice);
 }
