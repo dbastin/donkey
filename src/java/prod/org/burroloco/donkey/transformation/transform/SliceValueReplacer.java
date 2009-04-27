eee3f42c1cac5776358bbea02b0af6b18f8c6ae0
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-27 08:05:50 +0000

    Coverage.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@199 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/donkey/transformation/transform/SliceValueReplacer.java
+++
@@ -1,7 +0,0 @@
-package org.burroloco.donkey.transformation.transform;
-
-import org.burroloco.donkey.data.cake.Slice;
-
-public interface SliceValueReplacer {
-    String replace(String str, Slice slice, String key);
-}
