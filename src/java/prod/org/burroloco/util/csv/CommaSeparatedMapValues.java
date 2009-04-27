754be961526b9764b24e956e6751a9efa9c0baca
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-27 06:44:42 +0000

    Coverage.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@184 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/util/csv/CommaSeparatedMapValues.java
+++
@@ -1,9 +0,0 @@
-package org.burroloco.util.csv;
-
-import au.net.netstorm.boost.gunge.collection.StrictMap;
-
-import java.util.List;
-
-public interface CommaSeparatedMapValues {
-    List<String> list(StrictMap<String, String> map, String[] keys);
-}
