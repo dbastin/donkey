501145461da79faff79188cf861dc773aa993413
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-27 08:01:50 +0000

    Coverage.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@197 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ src/java/test/org/burroloco/util/string/DefaultStringUniqueAtomicTest.java
@@ -0,0 +1,36 @@
+package org.burroloco.util.string;
+
+import au.net.netstorm.boost.spider.api.runtime.Nu;
+import org.burroloco.test.glue.testcase.DonkeyTestCase;
+
+public class DefaultStringUniqueAtomicTest extends DonkeyTestCase {
+    Nu nu;
+
+    public void testAlwaysStamped() {
+        StringUnique subject = nu.nu(StringUnique.class, true, 2, 4);
+        String first = subject.unique("F");
+        assertEquals("F01", first);
+    }
+
+    public void testTruncated() {
+        StringUnique subject = nu.nu(StringUnique.class, true, 2, 6);
+        String first = subject.unique("FRED SEEMS LIKE SUCH A SHORT NAME");
+        assertEquals("FRED01", first);
+    }
+
+    public void testAlwaysStampedWithClash() {
+        StringUnique subject = nu.nu(StringUnique.class, true, 2, 4);
+        String first = subject.unique("FRED");
+        String second = subject.unique("FRED SEEMS LIKE SUCH A SHORT NAME");
+        assertEquals(4, second.length());
+        assertNotEquals(first, second);
+    }
+
+    public void testStampOnlyOnClash() {
+        StringUnique subject = nu.nu(StringUnique.class, false, 2, 4);
+        String first = subject.unique("FRED");
+        assertEquals("FRED", first);
+        String second = subject.unique("FRED");
+        assertNotEquals(first, second);
+    }
+}
