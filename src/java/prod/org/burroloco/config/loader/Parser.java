e5ea115ee0b0e0f05d15448c518fb7f6882caddb
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-28 05:19:42 +0000

    Removed cyclic package dependencies.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@233 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/config/loader/Parser.java
+++
@@ -1,7 +0,0 @@
-package org.burroloco.config.loader;
-
-import au.net.netstorm.boost.bullet.incredibles.core.Strong;
-
-public interface Parser {
-    <T extends Strong> T parse(Class<T> type, String value);
-}
