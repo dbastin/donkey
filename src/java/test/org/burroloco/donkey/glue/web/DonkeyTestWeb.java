ea48777c48ac157181f9c8e425c5b24a806218e3
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-28 04:13:41 +0000

    Testing webs simplified.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@227 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/test/org/burroloco/donkey/glue/web/DonkeyTestWeb.java
+++ src/java/test/org/burroloco/donkey/glue/web/DonkeyTestWeb.java
@@ -1,13 +1,29 @@
 package org.burroloco.donkey.glue.web;
 
-import au.net.netstorm.boost.spider.api.builder.Spinneret;
+import au.net.netstorm.boost.sniper.lifecycle.TestLifecycleBlocks;
 import au.net.netstorm.boost.spider.api.config.web.Web;
+import au.net.netstorm.boost.spider.api.config.wire.Wire;
+import au.net.netstorm.boost.spider.api.runtime.Impl;
+import au.net.netstorm.boost.spider.api.runtime.Nu;
+import au.net.netstorm.boost.spider.plugs.provider.core.Provider;
+import edge.org.dbunit.database.IDatabaseConnection;
+import org.burroloco.butcher.util.dbunit.IDatabaseConnectionProvider;
+import org.burroloco.donkey.cache.CachingProvider;
+import org.burroloco.donkey.glue.block.DonkeyTestLifecycleBlocks;
 
 public class DonkeyTestWeb implements Web {
-    Spinneret spinneret;
+    Impl impl;
+    Wire wire;
+    Nu nu;
 
     public void web() {
-        // FIX TSR-DONKEY Inline these instead...
-        spinneret.spin(SniperOverridesWeb.class, IConnectionCacheWeb.class);
+        wire.cls(DonkeyTestLifecycleBlocks.class).to(TestLifecycleBlocks.class);
+        connections();
+    }
+
+    private void connections() {
+        IDatabaseConnectionProvider reals = impl.impl(IDatabaseConnectionProvider.class);
+        Provider cached = nu.nu(CachingProvider.class, reals);
+        wire.provider(cached).to(IDatabaseConnection.class);
     }
 }
