3dcfd89ff0ac16596da1357f809a999b154d52a1
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-28 00:14:16 +0000

    Cleaned up fixes.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@209 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/test/org/burroloco/test/glue/block/DonkeyTestLifecycleBlocks.java
+++ src/java/test/org/burroloco/test/glue/block/DonkeyTestLifecycleBlocks.java
@@ -10,7 +10,7 @@ public class DonkeyTestLifecycleBlocks implements TestLifecycleBlocks {
     ArrayMaster arrays = new DefaultArrayMaster();
 
     public Class[] pre() {
-        Class[] before = {BasedirBlock.class, WireConfigBlock.class};
+        Class[] before = {BasedirBlock.class};
         Class[] boost = delegate.pre();
         Class[] after = {InsertDataBlock.class};
         return arrays.plus(arrays.plus(before, boost), after);
