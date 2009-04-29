44daebbfe674c98389c4052ece620fbdc553cee5
mcallana <mcallana@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-29 06:09:33 +0000

    donkey.bat
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@237 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/sh/prod/donkey.bat
+++ src/sh/prod/donkey.bat
@@ -1,21 +1,20 @@
 setLocal EnableDelayedExpansion
 
-set SCRIPT_DIR=%~dp0
+set SCRIPT_DIR=%CD%
 set SPECIFICATION_CLASSNAME=%1
 set CONFIG_DIR=%SCRIPT_DIR%\config
 set JAVA_CMD=%JAVA_HOME%\bin\java
 
 rmdir C:\temp\donkey
 mkdir C:\temp\donkey
-cp %CONFIG_DIR%\log4j\log4j.properties C:\temp\donkey
-cp donkey.jar c:\temp\donkey
-FOR /F "usebackq" %%i IN (`dir /b /s .\lib\*.jar`) DO cp %%i C:\temp\donkey
+copy donkey.jar c:\temp\donkey
+FOR /F "usebackq" %%i IN (`dir /b /s .\lib\*.jar`) DO copy %%i C:\temp\donkey
 
-set CLASSPATH="C:\TEMP\donkey\*"
+set CLASSPATH=%CLASSPATH%;"C:\TEMP\donkey\*"
 echo %CLASSPATH%
 set JVM_OPTIONS=-Xmx256m
 REM set JVM_DEBUG_OPTIONS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5010"
-set EXEC="%JAVA_CMD%" %JVM_OPTIONS% -Dlog4j.configuration=file:C:\TEMP\donkey\log4j.properties -classpath %CLASSPATH% org.burroloco.donkey.trebuchet.Main %SPECIFICATION_CLASSNAME%
+set EXEC="%JAVA_CMD%" %JVM_OPTIONS% -Dlog4j.configuration=file:%CONFIG_DIR%\log4j\log4j.properties -classpath %CLASSPATH% org.burroloco.donkey.trebuchet.Main %SPECIFICATION_CLASSNAME%
 
 echo %EXEC%
 %EXEC%
