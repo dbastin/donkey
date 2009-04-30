713a54df8692103f69167daeff1afbdfe1eb7f78
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-30 06:40:56 +0000

    HttptoJdbcDemoTest
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@246 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/donkey/job/HttpListenerJob.java
+++ src/java/prod/org/burroloco/donkey/job/HttpListenerJob.java
@@ -1,26 +1,22 @@
 package org.burroloco.donkey.job;
 
-import au.net.netstorm.boost.gunge.lifecycle.Stop;
 import au.net.netstorm.boost.spider.api.runtime.Nu;
+import edge.winstone.Launcher;
 import edge.winstone.LauncherStatic;
 import org.burroloco.config.core.Config;
-import edge.winstone.Launcher;
 
 import java.util.Map;
 
-public class HttpListenerJob implements Job, Stop {
-    private Launcher winstone;
+public class HttpListenerJob implements Job {
     LauncherStatic launcherStatic;
     Map args;
     Nu nu;
 
+    // FIX DONKEY WIP. Please leave.
     public void go(Config config) {
         args.put("webroot", "donkey");
+        args.put("httpPort", "8282");
         launcherStatic.initLogger(args);
-        winstone = nu.nu(Launcher.class, args);
-    }
-
-    public void stop() {
-        winstone.shutdown();
+        nu.nu(Launcher.class, args);
     }
 }
