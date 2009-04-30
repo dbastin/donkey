56451707746fc041015fdf4c3f594b9312c5511a
mcallana <mcallana@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-30 02:39:37 +0000

    donkey.bat - allow spaces in paths
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@240 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/sh/prod/donkey.bat
+++ src/sh/prod/donkey.bat
@@ -5,16 +5,18 @@ set SPECIFICATION_CLASSNAME=%1
 set CONFIG_DIR=%SCRIPT_DIR%\config
 set JAVA_CMD=%JAVA_HOME%\bin\java
 
+REM Copy all jars to one location to keep CLASSPATH small
 rmdir C:\temp\donkey
 mkdir C:\temp\donkey
 copy donkey.jar c:\temp\donkey
-FOR /F "usebackq" %%i IN (`dir /b /s .\lib\*.jar`) DO copy %%i C:\temp\donkey
+FOR /F "usebackq tokens=*" %%i IN (`dir /b /s .\lib\*.jar`) DO copy "%%i" C:\temp\donkey
 
-set CLASSPATH=%CLASSPATH%;"C:\TEMP\donkey\*"
+REM Requires Java 1.6
+set CLASSPATH=%CLASSPATH%;C:\TEMP\donkey\*
 echo %CLASSPATH%
 set JVM_OPTIONS=-Xmx256m
 REM set JVM_DEBUG_OPTIONS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5010"
-set EXEC="%JAVA_CMD%" %JVM_OPTIONS% -Dlog4j.configuration=file:%CONFIG_DIR%\log4j\log4j.properties -classpath %CLASSPATH% org.burroloco.donkey.trebuchet.Main %SPECIFICATION_CLASSNAME%
+set EXEC="%JAVA_CMD%" %JVM_OPTIONS% -Dlog4j.configuration=file:"%CONFIG_DIR%\log4j\log4j.properties" -classpath %CLASSPATH% org.burroloco.donkey.trebuchet.Main %SPECIFICATION_CLASSNAME%
 
 echo %EXEC%
 %EXEC%
