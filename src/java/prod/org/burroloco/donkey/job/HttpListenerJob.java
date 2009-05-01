f0df6528eb4a46b2f098949e6ced8ff93dc77031
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-01 11:46:35 +0000

    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@285 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/donkey/job/HttpListenerJob.java
+++ src/java/prod/org/burroloco/donkey/job/HttpListenerJob.java
@@ -1,8 +1,8 @@
 package org.burroloco.donkey.job;
 
+import au.net.netstorm.boost.gunge.lifecycle.Stop;
 import au.net.netstorm.boost.spider.api.runtime.Impl;
 import au.net.netstorm.boost.spider.api.runtime.Nu;
-import au.net.netstorm.boost.gunge.lifecycle.Stop;
 import edge.org.mortbay.jetty.Server;
 import org.burroloco.config.core.Config;
 import org.burroloco.config.core.WeakConfig;
@@ -18,7 +18,7 @@ public class HttpListenerJob implements Job, Stop {
 
     public void go(Config config) {
         server = server(config);
-        setHandler(config, server);
+        setHandler(config);
         server.start();
         server.join();
     }
@@ -32,7 +32,7 @@ public class HttpListenerJob implements Job, Stop {
         return nu.nu(Server.class, port);
     }
 
-    private void setHandler(Config config, Server server) {
+    private void setHandler(Config config) {
         AbstractHandler handler = impl.impl(JettyRequestHandler.class, config);
         server.setHandler(handler);
     }
