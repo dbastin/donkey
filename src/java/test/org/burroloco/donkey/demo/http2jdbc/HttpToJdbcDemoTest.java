42c184c7f4c2bfb1bd9253fe29e529219b095a3e
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-01 01:52:43 +0000

    HttptoJdbcDemoTest
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@264 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/test/org/burroloco/donkey/demo/http2jdbc/HttpToJdbcDemoTest.java
+++ src/java/test/org/burroloco/donkey/demo/http2jdbc/HttpToJdbcDemoTest.java
@@ -1,8 +1,6 @@
 package org.burroloco.donkey.demo.http2jdbc;
 
-import au.net.netstorm.boost.bullet.scalpel.engine.Edger;
 import au.net.netstorm.boost.spider.api.runtime.Impl;
-import au.net.netstorm.boost.spider.api.runtime.Nu;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
@@ -17,16 +15,13 @@ import java.io.IOException;
 public class HttpToJdbcDemoTest extends DonkeyTestCase {
     Trebuchet trebuchet;
     Snoozer snoozer;
-    Edger edger;
     Impl impl;
-    Nu nu;
 
     // FIX DONKEY WIP Please leave... 
     public void testHttpToJdbc() throws IOException {
         trebuchet.launch(HttpToJdbcSpecification.class);
         snoozer.snooze(2000);
         sendMessage("This is the best thing since sliced bread");
-        snoozer.snooze(2000);
     }
 
     private void sendMessage(String s) throws IOException {
