0ad01f7224fcf2cd87397ca49210ca91b3a7bc2b
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-01 10:34:22 +0000

    HttpToCsvDemoTest
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@281 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/test/org/burroloco/donkey/demo/http2jdbc/HttpToCsvDemoTest.java
+++ src/java/test/org/burroloco/donkey/demo/http2jdbc/HttpToCsvDemoTest.java
@@ -2,7 +2,6 @@ package org.burroloco.donkey.demo.http2jdbc;
 
 import au.net.netstorm.boost.gunge.lifecycle.Stop;
 import au.net.netstorm.boost.sniper.marker.Destroyable;
-import au.net.netstorm.boost.spider.api.runtime.Impl;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -10,7 +9,6 @@ import org.burroloco.butcher.util.file.FileComparator;
 import org.burroloco.donkey.glue.testcase.DonkeyTestCase;
 import org.burroloco.donkey.job.Job;
 import org.burroloco.donkey.trebuchet.TestTrebuchet;
-import org.burroloco.util.snooze.Snoozer;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,12 +18,9 @@ public class HttpToCsvDemoTest extends DonkeyTestCase implements Destroyable {
     private static final File ACTUAL = new File("gen/demo/out/news.csv");
     FileComparator comparator;
     TestTrebuchet trebuchet;
-    Snoozer snoozer;
-    Impl impl;
 
     public void testHttpToCsv() throws IOException {
         trebuchet.launch(HttpToCsvSpecification.class, HttpServerTestWeb.class);
-        snoozer.snooze(1000);
         sendMessage();
         comparator.assertEquals(EXPECTED, ACTUAL);
     }
