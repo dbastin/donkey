5ba8d0403be860e143f157384d14573b615714af
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-01 01:27:40 +0000

    HttptoJdbcDemoTest
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@259 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/donkey/input/http/EdgeHttpServlet.java
+++ src/java/prod/org/burroloco/donkey/input/http/EdgeHttpServlet.java
@@ -1,27 +1,13 @@
 package org.burroloco.donkey.input.http;
 
-import au.net.netstorm.boost.bullet.scalpel.core.Edges;
-
 import javax.servlet.ServletException;
 import java.io.IOException;
 
 public class EdgeHttpServlet extends javax.servlet.http.HttpServlet {
-    // HttpServlet delegate;
-    Edges edger;
-
     // OK ThrowsCount {
     public void doPost(javax.servlet.http.HttpServletRequest request,
                        javax.servlet.http.HttpServletResponse response) throws ServletException, IOException {
         // FIX TSR-DONKEY Implement...
-//        delegate.doPost(edge(request), edge(response));
     }
     // } ThrowsCount
-
-//    private HttpServletRequest edge(javax.servlet.http.HttpServletRequest request) {
-//        return edger.edge(HttpServletRequest.class, request);
-//    }
-//
-//    private HttpServletResponse edge(javax.servlet.http.HttpServletResponse response) {
-//        return edger.edge(HttpServletResponse.class, response);
-//    }
 }
