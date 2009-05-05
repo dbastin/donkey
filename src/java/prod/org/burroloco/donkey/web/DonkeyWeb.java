36fa4e603897117b357637c8c0947c14f139d68c
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-05 01:16:32 +0000

    Extending use of spider scope for 3rd party use.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@295 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/donkey/web/DonkeyWeb.java
+++ src/java/prod/org/burroloco/donkey/web/DonkeyWeb.java
@@ -4,18 +4,20 @@ import au.net.netstorm.boost.spider.api.builder.Spinneret;
 import au.net.netstorm.boost.spider.api.config.factory.Factorer;
 import au.net.netstorm.boost.spider.api.config.mapping.Mapper;
 import au.net.netstorm.boost.spider.api.config.web.Web;
+import au.net.netstorm.boost.spider.api.config.scope.Scoper;
 import au.net.netstorm.boost.spider.plugs.factory.supplied.ThreadedFactory;
 import org.burroloco.donkey.cache.ConnectionCacheWeb;
 import org.burroloco.donkey.log.LoggingWeb;
 
 public class DonkeyWeb implements Web {
-    String scope = "org.burroloco";
     Spinneret spinneret;
     Factorer factorer;
     Mapper mapper;
+    Scoper scoper;
 
     public void web() {
-        mapper.prefix("Default", scope);
+        scoper.scope("org.burroloco");
+        mapper.prefix("Default");
         factorer.add(ThreadedFactory.class);
         spinneret.spin(LoggingWeb.class, ConnectionCacheWeb.class);
     }
