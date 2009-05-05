8dbaf7619c0beb560ea6d1f6de2e8dbf93776312
dbastin <dbastin@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-05 04:44:53 +0000

    Extending use of spider scope for 3rd party use.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@299 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/prod/org/burroloco/donkey/web/DefaultExtraScoper.java
+++ src/java/prod/org/burroloco/donkey/web/DefaultExtraScoper.java
@@ -1,47 +1,16 @@
 package org.burroloco.donkey.web;
 
 import au.net.netstorm.boost.bullet.log.Log;
-import au.net.netstorm.boost.gunge.collection.StrictMap;
-import au.net.netstorm.boost.spider.api.config.mapping.Mapper;
-import au.net.netstorm.boost.spider.api.config.scope.Scoper;
-import au.net.netstorm.boost.spider.api.runtime.Impl;
-import org.burroloco.config.loader.DefaultPropertiesLoader;
-import org.burroloco.config.loader.PropertiesLoader;
 
 import java.io.File;
-import java.util.Set;
 
 public class DefaultExtraScoper implements ExtraScoper {
-    Mapper mapper;
-    Scoper scoper;
-    Impl impl;
+    ExtraScoperEngine engine;
     Log log;
 
     public void scope(String fileName) {
         if (!hasExtras(fileName)) log(fileName);
-        else addExtras(fileName);
-    }
-
-    private void addExtras(String extras) {
-        StrictMap<String, String> scope = load(extras);
-        Set<String> packageNames = scope.keySet();
-        for (String name : packageNames) add(name, scope.get(name));
-    }
-
-    private StrictMap<String, String> load(String extras) {
-        PropertiesLoader loader = impl.impl(DefaultPropertiesLoader.class);
-        return loader.load(extras);
-    }
-
-    private void add(String pkg, String impls) {
-        scoper.scope(pkg);
-        pattern(impls);
-    }
-
-    private void pattern(String pattern) {
-        String impl = pattern.replaceAll("\\*", "");
-        if (pattern.startsWith("*")) mapper.suffix(impl);
-        if (pattern.endsWith("*")) mapper.prefix(impl);
+        else engine.scope(fileName);
     }
 
     private boolean hasExtras(String extras) {
