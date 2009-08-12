68f8789817fe0fd7e83406b5496bd300ee8fda87
dchappell <dchappell@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-08-12 05:41:26 +0000

    Extra options in the props file relating to FTP details
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@347 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ src/java/prod/org/burroloco/donkey/config/FtpName.java
@@ -0,0 +1,8 @@
+package org.burroloco.donkey.config;
+
+import au.net.netstorm.boost.bullet.incredibles.core.Strong;
+
+
+public interface FtpName extends Strong<String> {
+    String NAME = "ftp.name";
+}
