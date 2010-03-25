244019fefe5821cbd066334ec44cd43070e75a5f
dchappell <dchappell@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-03-25 06:34:47 +0000

    Dave's birthday everyday.. take two
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@351 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/java/test/org/burroloco/butcher/glue/constants/ButcherTestConstants.java
+++ src/java/test/org/burroloco/butcher/glue/constants/ButcherTestConstants.java
@@ -6,5 +6,5 @@ public interface ButcherTestConstants {
     String DAVES_BIRTHDAYTIME_SLASH = "1980/01/24 12:00:00";
     String DAVES_BIRTHDAY_DASH = "1980-01-24";
     String DATE_PATTERN_SLASH_DMY = "(?m)\\d{2}/\\d{2}/\\d{4}";
-    String DATE_PATTERN_SLASH_YMDHMS = "(?m)\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}";
+    String DATE_PATTERN_SLASH_YMDHMS = "(?m)\\d{4}/\\d{2}/\\d{2} \\d{2}:\\d{2}:\\d{2}";
 }
