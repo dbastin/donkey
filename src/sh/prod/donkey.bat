5162f60704620ce1c8e8afe891a5ea672b240f79
mcallana <mcallana@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2011-04-11 06:45:30 +0000

    Make donkey.bat behave a little nicer with other donkey.bats running concurrently. Bump up -Xmx.
    git-svn-id: https://subversion.assembla.com/svn/burro/trunk@352 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/sh/prod/donkey.bat
+++ src/sh/prod/donkey.bat
@@ -6,17 +6,20 @@ set CONFIG_DIR=%SCRIPT_DIR%\config
 set JAVA_CMD=%JAVA_HOME%\bin\java
 
 REM Copy all jars to one location to keep CLASSPATH small
-rmdir C:\temp\donkey
-mkdir C:\temp\donkey
-copy donkey.jar c:\temp\donkey
-FOR /F "usebackq tokens=*" %%i IN (`dir /b /s .\lib\*.jar`) DO copy "%%i" C:\temp\donkey
+rmdir /S /Q C:\temp\donkey_%SPECIFICATION_CLASSNAME%
+mkdir C:\temp\donkey_%SPECIFICATION_CLASSNAME%
+copy donkey.jar c:\temp\donkey_%SPECIFICATION_CLASSNAME%
+FOR /F "usebackq tokens=*" %%i IN (`dir /b /s .\lib\*.jar`) DO copy "%%i" C:\temp\donkey_%SPECIFICATION_CLASSNAME%
 
 REM Requires Java 1.6
-set CLASSPATH=%CLASSPATH%;C:\TEMP\donkey\*
+set CLASSPATH=%CLASSPATH%;C:\temp\donkey_%SPECIFICATION_CLASSNAME%\*
 echo %CLASSPATH%
-set JVM_OPTIONS=-Xmx256m
+set JVM_OPTIONS=-Xmx512m
 REM set JVM_DEBUG_OPTIONS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5010"
 set EXEC="%JAVA_CMD%" %JVM_OPTIONS% -Dlog4j.configuration=file:"%CONFIG_DIR%\log4j\log4j.properties" -classpath %CLASSPATH% org.burroloco.donkey.trebuchet.Main %SPECIFICATION_CLASSNAME%
 
 echo %EXEC%
 %EXEC%
+
+REM Cleanup
+rmdir /S /Q C:\temp\donkey_%SPECIFICATION_CLASSNAME%
