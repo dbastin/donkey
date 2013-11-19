be5ff10590572c9b63578d8f7daa2fb9ee6a068d
Damien Bastin <damien.bastin@gmail.com>
2013-11-19 15:35:24 +1000

    Removed the TemplateSelector. Unnecessary.

--- src/java/prod/org/burroloco/donkey/spit/template/DefaultTemplateExpander.java
+++ src/java/prod/org/burroloco/donkey/spit/template/DefaultTemplateExpander.java
@@ -1,14 +1,16 @@
 package org.burroloco.donkey.spit.template;
 
 import org.burroloco.config.core.Config;
+import org.burroloco.config.core.WeakConfig;
+import org.burroloco.donkey.config.OutputTemplate;
 import org.burroloco.donkey.data.core.Tuple;
 
 public class DefaultTemplateExpander implements TemplateExpander {
-    TemplateSelector selector;
     Templator templator;
+    WeakConfig weak;
 
     public String expand(Config config, Tuple tuple) {
-        String template = selector.select(config, tuple);
+        String template = weak.get(config, OutputTemplate.class);
         return templator.apply(tuple, template);
     }
 }
