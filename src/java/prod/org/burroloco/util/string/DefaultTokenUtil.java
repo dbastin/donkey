0983bf68722b5f6de8336f46c3a6056e9ae6d1e9
Damien Bastin <damien.bastin@gmail.com>
2013-11-20 19:46:46 +1000

    Made the token replacement on the recorder sql case insensitive.

--- src/java/prod/org/burroloco/util/string/DefaultTokenUtil.java
+++ src/java/prod/org/burroloco/util/string/DefaultTokenUtil.java
@@ -3,13 +3,15 @@ package org.burroloco.util.string;
 import java.util.Map;
 
 public class DefaultTokenUtil implements TokenUtil {
+    private static final String IGNORE_CASE = "(?i)";
+
     Tokenizer tokenizer;
 
     public String replace(String input, Map<String, String> replacements) {
         String output = input;
         for (String was : replacements.keySet()) {
             String now = replacements.get(was);
-            String token = tokenizer.token(was);
+            String token = IGNORE_CASE + tokenizer.token(was);
             output = output.replaceAll(token, now);
         }
         return output;
