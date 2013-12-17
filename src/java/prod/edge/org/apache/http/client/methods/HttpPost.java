d2e697cb564dc13a9176c6a2d29d9b36a4e8c36e
Damien Bastin <damien.bastin@gmail.com>
2013-12-17 16:42:27 +1000

    Added edge method.

--- src/java/prod/edge/org/apache/http/client/methods/HttpPost.java
+++ src/java/prod/edge/org/apache/http/client/methods/HttpPost.java
@@ -1,6 +1,8 @@
 package edge.org.apache.http.client.methods;
 
 import au.net.netstorm.boost.bullet.scalpel.core.Edge;
+import org.apache.http.Header;
 
 public interface HttpPost extends Edge, HttpEntityEnclosingRequestBase {
+    Header[] getHeaders(String name);
 }
