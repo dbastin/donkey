36309f5460d807757b4e77bcdb3e8119108d5a0d
Damien Bastin <damien.bastin@gmail.com>
2013-12-18 15:54:48 +1000

    Just amending the HttpRequest param values to be Strings

--- src/java/test/org/burroloco/butcher/fixture/http/HttpRequest.java
+++ src/java/test/org/burroloco/butcher/fixture/http/HttpRequest.java
@@ -6,15 +6,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class HttpRequest extends Primordial {
-    Map<String, Object> params = new HashMap<String, Object>();
+    Map<String, String> params = new HashMap<String, String>();
     String payload;
 
-    public HttpRequest(Map<String, Object> params, String payload) {
+    public HttpRequest(Map<String, String> params, String payload) {
         this.params = params;
         this.payload = payload;
     }
 
-    public Map<String, Object> getParams() {
+    public Map<String, String> getParams() {
         return params;
     }
 
